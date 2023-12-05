6fa063b3073cc2153bab738b65a5588be7625c7f
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-05 12:38:30 +0200

    Create ExcellentResult.java

---
+++ scr/_02_Conditional_Statements/Lab/ExcellentResult.java
@@ -0,0 +1,14 @@
+package scr._02_Conditional_Statements.Lab;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mark  = Integer.parseInt(scanner.nextLine());
+        if (mark >= 5){
+            System.out.println("Excellent!");
+        }
+
+    }
+}
