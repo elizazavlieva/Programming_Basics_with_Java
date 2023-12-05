de3790e85bcc6b86de3d3ba68a36bcc033cf81ff
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-05 13:02:48 +0200

    Create PasswordGuess.java

---
+++ scr/_02_Conditional_Statements/Lab/PasswordGuess.java
@@ -0,0 +1,16 @@
+package scr._02_Conditional_Statements.Lab;
+
+import java.util.Scanner;
+
+public class PasswordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String password = "s3cr3t!P@ssw0rd";
+        if (text.equals(password)){
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
