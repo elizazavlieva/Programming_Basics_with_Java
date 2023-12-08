df3821712005362e8b2cbf1460dbe161af3d86b4
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-08 10:11:20 +0200

    Create NumbersFrom1ToNWithStep3.java

---
+++ scr/_4_For_Loop/Lab/NumbersFrom1ToNWithStep3.java
@@ -0,0 +1,14 @@
+package scr._4_For_Loop.Lab;
+
+import java.util.Scanner;
+
+public class NumbersFrom1ToNWithStep3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i+=3){
+            System.out.println(i);
+        }
+    }
+}
