9950d972e731887bc7cf17bc0ee82cb1b296e0d3
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-08 10:01:30 +0200

    Create NumbersFrom1To100.java

---
+++ scr/_4_For_Loop/Exercises/NumbersFrom1To100.java
@@ -0,0 +1,10 @@
+package scr._4_For_Loop.Exercises;
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
