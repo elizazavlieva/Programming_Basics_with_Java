2a296d50e347345d19bc93c23d42f32b9dd64a5d
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-08 11:21:07 +0200

    Create NumbersEndingIn7.java

---
+++ scr/_4_For_Loop/Exercises/NumbersEndingIn7.java
@@ -0,0 +1,12 @@
+package scr._4_For_Loop.Exercises;
+
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++){
+            if (i % 10 == 7 ){
+                System.out.println(i);
+            }
+        }
+    }
+}
