16f6a96534ffc5d06f154e931d1762f186f92bb6
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-08 10:05:37 +0200

    Create NumbersNTo1.java

---
+++ scr/_4_For_Loop/Lab/NumbersNTo1.java
@@ -0,0 +1,14 @@
+package scr._4_For_Loop.Lab;
+
+import java.util.Scanner;
+
+public class NumbersNTo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = n; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
