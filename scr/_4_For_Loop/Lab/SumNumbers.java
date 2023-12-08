89943988403260eca1fba3b5b175f6ad4c6b7d63
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-08 10:44:55 +0200

    Create SumNumbers.java

---
+++ scr/_4_For_Loop/Lab/SumNumbers.java
@@ -0,0 +1,17 @@
+package scr._4_For_Loop.Lab;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int result = 0;
+
+        for (int i = 0; i < n ; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            result += number;
+        }
+        System.out.println(result);
+    }
+}
